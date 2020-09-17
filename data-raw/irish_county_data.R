f07b940bb44e68889b91367f00005d1ffa410d5b
williamorim <thewilliam89@gmail.com>
2020-09-16 22:14:22 -0300

    ireland county map

---
+++ data-raw/irish_county_data.R
@@ -0,0 +1,15 @@
+## code to prepare `irish_county_data` dataset goes here
+
+geo_irish_county <- sf::st_read("data-raw/geojson/counties_simple.geojson")
+
+irish_county_data <- readr::read_rds(
+  'data-raw/rds/latest_irish_county_data.rds'
+) %>%
+  dplyr::filter(Date == max(Date)) %>%
+  dplyr::left_join(
+    geo_irish_county,
+    by = c("CountyName" = "NAME_TAG")
+  ) %>%
+  sf::st_as_sf()
+
+usethis::use_data(irish_county_data, overwrite = TRUE)
